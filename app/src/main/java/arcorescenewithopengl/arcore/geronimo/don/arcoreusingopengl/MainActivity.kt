5484db4aecb34259d7a279ec46893cf15a25d657
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2018-11-05 10:30:52 -0200

    Faltou arquivo

---
+++ app/src/main/java/arcorescenewithopengl/arcore/geronimo/don/arcoreusingopengl/MainActivity.kt
@@ -0,0 +1,12 @@
+package arcorescenewithopengl.arcore.geronimo.don.arcoreusingopengl
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
